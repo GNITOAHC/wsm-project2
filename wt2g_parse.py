8bd1d35ad054a7d28e39fb9061a4bfaa2d64aab9
GINTOAHC <chaotingchen10@gmail.com>
2024-11-27 01:56:02 +0800

    feat: Calc mean and variance

--- wt2g_parse.py
+++ wt2g_parse.py
@@ -6,11 +6,13 @@ OUTPUT_FILE = "./data/collection/collections.jsonl"
 
 
 count = 0
+l = []
 
 
 def parse_single_file(file_path):
     documents = []
     global count
+    global l
     with open(file_path, "rb") as f:
         content = f.read().decode("utf-8", errors="ignore")
         raw_docs = content.split("<DOC>")[1:]
@@ -19,6 +21,7 @@ def parse_single_file(file_path):
         for doc in raw_docs:
             doc_id = doc.split("<DOCNO>")[1].split("</DOCNO>")[0].strip()
             documents.append({"id": doc_id, "contents": doc})
+            l.append(len(doc))
             count += 1
     return documents
 
@@ -36,6 +39,12 @@ if __name__ == "__main__":
             file_path = os.path.join(dirpath, filename)
             documents = parse_single_file(file_path)
             write_to_jsonl(documents)
+
+    mean = sum(l) / len(l)
+    variance = sum([((x - mean) ** 2) for x in l]) / len(l)
+
+    print("Mean: ", mean)
+    print("Variance: ", variance)
 
     if count != 247491:
         print("Error: count != 247491")
